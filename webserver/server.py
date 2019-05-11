33cc6e6636a6e98ef04e5642ddfe67751e9f0563
root <zkalmar@suse.com>
2019-05-11 12:18:47 +0200

    Add radio functions

--- webserver/server.py
+++ webserver/server.py
@@ -1,73 +1,62 @@
-'''
+#!/usr/bin/env python3
 
-Adapted excerpt from Getting Started with Raspberry Pi by Matt Richardson
-
-Modified by Rui Santos
-Complete project details: https://randomnerdtutorials.com
-
-'''
-
-import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
-GPIO.setmode(GPIO.BCM)
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
-   24 : {'name' : 'GPIO 24', 'state' : GPIO.LOW}
-   }
-
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+import sqlite3 as sql
+import stream as stream
+import volume as volume
 
 @app.route("/")
 def main():
-    templateData = {}
-    return render_template('index.html', **templateData)
+    conn = sql.connect("radio.db")
+    conn.row_factory = sql.Row
+    cur = conn.cursor()
+    cur.execute("select id, logo from stations;")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template("index.html",rows = rows)
 
-@app.route("/control")
-def control():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('control.html', **templateData)
+@app.route('/turn_off')
+def turn_off():
+    print ("turn off")
+    stream.stop()
+    return ("nothing")
 
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/control/<changePin>/<action>")
-def action(changePin, action):
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+@app.route('/voldn')
+def voldn():
+    print ("volume down")
+    volume.voldn()
+    return ("nothing")
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+@app.route('/volup')
+def volup():
+    print ("volume up")
+    volume.volup()
+    return ("nothing")
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins
-   }
+@app.route('/mute')
+def mute():
+    print ("mute")
+    volume.mute
+    return ("nothing")
 
-   return render_template('control.html', **templateData)
+@app.route('/station/<station_id>')
+def station(station_id):
+    print (station_id)
+    conn = sql.connect("radio.db")
+    conn.row_factory = sql.Row
+    cur = conn.cursor()
+    cur.execute("select url from stations where id="+station_id+" limit 1;")
+    conn.commit()
+    rows=cur.fetchall()
+    for url in rows:
+        print (url[0])
+    stream.play(url[0])
+    return ("nothing")
+
+@app.route("/admin")
+def admin():
+   return render_template('control.html')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
--- webserver/stream.py
+++ webserver/stream.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
-import sqlite3
-url = 'http://prem1.rockradio.com:80/bluesrock?9555ae7caa92404c73cade1d'
-instance=vlc.Instance()
-media=vlc.MediaPlayer(url)
-media.play()
+import subprocess
+
+def play(url):
+    subprocess.Popen(["/usr/bin/mplayer", url])
+
+def stop():
+    subprocess.Popen(["/usr/bin/pkill", "mplayer"])
